70324bb58c0842aeb0dbe9c62ad5c2dffc40a992
Alex <jhosepgarbe0331@gmail.com>
2023-04-27 01:22:34 -0500

    creacion y subida del repositorio del sistema de ventas casi terminado

---
+++ config/Config.php
@@ -0,0 +1,14 @@
+<?php
+define('BASE_URL', 'http://192.168.1.40/pos/');
+define('HOST', 'localhost');
+define('USER', 'root');
+define('PASS', '');
+define('DBNAME', 'sistema');
+define('CHARSET', 'charset=utf8');
+define('TITLE', 'POS VENTA');
+define('MONEDA', 'S/ ');
+define('HOST_SMTP', 'smtp.gmail.com');
+define('USER_SMTP', 'tucorreo@gmail.com');
+define('CLAVE_SMTP', 'clave_generada');
+define('PUERTO_SMTP', 465);
+?>
